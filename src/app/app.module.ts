dd045bca84adb06f9603ca169aea1f32ae41b049
Sergio Raposo Vargas <sraposov@everis.com>
2017-05-17 23:10:08 +0200

    pruebas primeng y creación del fichero scss/styles.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MessagesModule } from 'primeng/primeng';
+import { ButtonModule } from 'primeng/primeng';
 
+//Bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +16,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule,
+    MessagesModule, //PrimeNG
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
